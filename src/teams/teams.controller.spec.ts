661d28d46f759481ee3751f2b2454a23bee9e7f2
Alisa Zavesa <alisa.zavesa@yandex.ru>
2026-03-10 23:06:23 +0300

    fix unit test controller (team)

--- src/teams/teams.controller.spec.ts
+++ src/teams/teams.controller.spec.ts
@@ -2,12 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
 import { CreateTeamDto } from './dto/create-team.dto';
-import { TeamResponseDto } from './dto/team-response.dto';
-
 
 describe('TeamsController', () => {
   let controller: TeamsController;
-  let service: TeamsService;
 
   const mockTeamsService = {
     create: jest.fn((dto: CreateTeamDto) => ({
@@ -15,27 +12,18 @@ describe('TeamsController', () => {
       name: dto.name,
       createdAt: new Date(),
       deletedAt: null,
-    } as TeamResponseDto)
-  ),
-    findAll: jest.fn(),
-    findOne: jest.fn(),
-    update: jest.fn(),
-    remove: jest.fn(),
+    })),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeamsController],
-        providers: [
-          {
-            provide: TeamsService,
-            useValue: mockTeamsService,
-          },
-        ],
+      providers: [
+        { provide: TeamsService, useValue: mockTeamsService },
+      ],
     }).compile();
 
     controller = module.get<TeamsController>(TeamsController);
-    service = module.get<TeamsService>(TeamsService);
   });
 
   it('should be defined', () => {
